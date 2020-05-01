09db4b5a54140b0a05d2773040bef16172f71e8f
Daniel Barter <daniel@mercury.com>
2020-05-01 16:17:01 -0700

    kill switch

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -8,19 +8,22 @@ import Network.Wai.Handler.Warp (run)
 import qualified Data.ByteString.Lazy as B
 import Data.ByteString.Char8 (unpack)
 import System.Process (callProcess)
+import System.Posix.Files
+import System.Exit
 
 
 app :: Application
 app request respond = do
-  appendFile "log" $ ( show $ remoteHost request ) <> "\n"
-  let queryString = "tealeaf::" <> ( unpack $ rawQueryString request )
-  callProcess "./generateTeaLeaf.elf" [queryString]
-  callProcess "convert" ["tealeaf.bmp","tealeaf.png"]
-  teaLeaf <- B.readFile "tealeaf.png"
-  respond $ responseLBS
-    status200
-      [("Content-Type", "image/png")]
-      teaLeaf
+  size <- fromIntegral . fileSize <$> getFileStatus "log"
+  if size > 10000
+    then exitFailure
+    else do appendFile "log" $ ( show $ remoteHost request ) <> "\n"
+            let queryString = "tealeaf::" <> ( unpack $ rawQueryString request )
+            callProcess "./generateTeaLeaf.elf" [queryString]
+            callProcess "convert" ["tealeaf.bmp","tealeaf.png"]
+            teaLeaf <- B.readFile "tealeaf.png"
+            respond $ responseLBS
+              status200 [("Content-Type", "image/png")] teaLeaf
 
 main :: IO ()
 main = do
